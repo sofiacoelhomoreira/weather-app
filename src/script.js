ca412427ab13a2401a5518bd8342231ed1bb80dc
sofiacoelhomoreira <72118018+sofiacoelhomoreira@users.noreply.github.com>
2021-07-13 13:23:01 -0300

    fixing time
    Fixe time for the hours and minutes <0 and more structure alterations

--- src/script.js
+++ src/script.js
@@ -1,19 +1,33 @@
-let now= new Date();
-let h2=document.querySelector("h2");
+
+
+function formatDate(now){
+    
+    let hours = now.getHours();
+    if (hours<10){
+        hours=`0${hours}`;
+    }
+let minutes = now.getMinutes();
+      if (minutes<10){
+        minutes=`0${minutes}`;
+    }
+
+
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day = days[now.getDay()];
 
-let hours = now.getHours();
-let minutes = now.getMinutes();
 
-h2.innerHTML = `${day}, ${hours}:${minutes}`;
+return `${day}, ${hours}:${minutes}`
+}
 
+let nowElement = document.querySelector("h1"); 
+let currentTime=new Date();
+nowElement.innerHTML=formatDate(currentTime);
 
 function search(event){
     event.preventDefault();
     let searchInput= document.querySelector("#search-text-input");
-    let h1 =document.querySelector("h1");
-    h1.innerHTML = `Search for ${searchInput.value}..`;
+    let h2 =document.querySelector("h2");
+    h2.innerHTML = `Search for ${searchInput.value}..`;
 }
 
 let form= document.querySelector("#search-form");
